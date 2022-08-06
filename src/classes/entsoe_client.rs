a104c3a1b55843f15d666d82a74f8c5eb9ca3b8a
johannes <johannes.mast@gmx.net>
2022-08-06 14:26:02 +0200

    added more query functions

--- src/classes/entsoe_client.rs
+++ src/classes/entsoe_client.rs
@@ -86,4 +86,79 @@ impl<'a> EntsoeClient<'a> {
         }
         self.basic_request(start_time, end_time, params)
     }
+
+    pub fn query_load_forecast(
+        &self,
+        start_time: &str,
+        end_time: &str,
+        area: &str,
+        process_type: Option<&str>,
+    ) -> Result<Vec<DatetimeValue>, Box<dyn Error>> {
+        let mut params: HashMap<&str, &str> = HashMap::new();
+        params.insert("documentType", "A65");
+        params.insert("processType", process_type.unwrap_or("A01"));
+        match Mappings::DOMAIN_MAPPINGS.get(area) {
+            Some(&domain_value) => {
+                params.insert("outBiddingZone_Domain", domain_value);
+            }
+            _ => println!("Don't have mapping for area."),
+        }
+        self.basic_request(start_time, end_time, params)
+    }
+
+    pub fn query_generation_forecast(
+        &self,
+        start_time: &str,
+        end_time: &str,
+        area: &str,
+        process_type: Option<&str>,
+    ) -> Result<Vec<DatetimeValue>, Box<dyn Error>> {
+        let mut params: HashMap<&str, &str> = HashMap::new();
+        params.insert("documentType", "A71");
+        params.insert("processType", process_type.unwrap_or("A01"));
+        match Mappings::DOMAIN_MAPPINGS.get(area) {
+            Some(&domain_value) => {
+                params.insert("outBiddingZone_Domain", domain_value);
+            }
+            _ => println!("Don't have mapping for area."),
+        }
+        self.basic_request(start_time, end_time, params)
+    }
+
+    pub fn query_wind_and_solar_forecast(
+        &self,
+        start_time: &str,
+        end_time: &str,
+        area: &str,
+        process_type: Option<&str>,
+    ) -> Result<Vec<DatetimeValue>, Box<dyn Error>> {
+        let mut params: HashMap<&str, &str> = HashMap::new();
+        params.insert("documentType", "A65");
+        params.insert("processType", process_type.unwrap_or("A01"));
+        match Mappings::DOMAIN_MAPPINGS.get(area) {
+            Some(&domain_value) => {
+                params.insert("outBiddingZone_Domain", domain_value);
+            }
+            _ => println!("Don't have mapping for area."),
+        }
+        self.basic_request(start_time, end_time, params)
+    }
+
+    pub fn query_generation(
+        &self,
+        start_time: &str,
+        end_time: &str,
+        area: &str,
+    ) -> Result<Vec<DatetimeValue>, Box<dyn Error>> {
+        let mut params: HashMap<&str, &str> = HashMap::new();
+        params.insert("documentType", "A75");
+        params.insert("processType", "A16");
+        match Mappings::DOMAIN_MAPPINGS.get(area) {
+            Some(&domain_value) => {
+                params.insert("in_Domain", domain_value);
+            }
+            _ => println!("Don't have mapping for area."),
+        }
+        self.basic_request(start_time, end_time, params)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
+use crate::classes::DatetimeValue;
 use crate::classes::EntsoeClient;
 use crate::classes::HelpFuncs;
+use std::error::Error;
 mod classes;
+
+enum QueryChoice {
+    QueryDayAheadPrices,
+    QueryNetPosition,
+    QueryLoad,
+    QueryLoadForecast,
+    QueryGenerationForecast,
+    QueryWindAndSolarForecast,
+    QueryGeneration,
+}
+
+enum DataChoice {
+    WriteToCsvFile,
+    WriteToJsonFile,
+    ShowAsGraph,
+}
 
 fn main() {
     const API_KEY: &str = "<API_KEY>";
@@ -9,8 +27,44 @@ fn main() {
     const AREA: &str = "DE_LU";
     const ENTSOE_CLIENT: EntsoeClient = EntsoeClient { api_key: API_KEY };
 
-    let _resp = ENTSOE_CLIENT.query_day_ahead_prices(START_TIME, END_TIME, AREA);
-    HelpFuncs::write_results_csv("test.csv", ";", _resp.unwrap());
-    let _resp = ENTSOE_CLIENT.query_net_position(START_TIME, END_TIME, AREA);
-    let _resp = ENTSOE_CLIENT.query_load(START_TIME, END_TIME, AREA);
+    let choice_query = QueryChoice::QueryDayAheadPrices;
+    let choice_data = DataChoice::WriteToCsvFile;
+
+    let resp: Result<Vec<DatetimeValue>, Box<dyn Error>>;
+
+    match choice_query {
+        QueryChoice::QueryDayAheadPrices => {
+            resp = ENTSOE_CLIENT.query_day_ahead_prices(START_TIME, END_TIME, AREA);
+        }
+        QueryChoice::QueryNetPosition => {
+            resp = ENTSOE_CLIENT.query_net_position(START_TIME, END_TIME, AREA);
+        }
+        QueryChoice::QueryLoad => {
+            resp = ENTSOE_CLIENT.query_load(START_TIME, END_TIME, AREA);
+        }
+        QueryChoice::QueryLoadForecast => {
+            resp = ENTSOE_CLIENT.query_load_forecast(START_TIME, END_TIME, AREA, None);
+        }
+        QueryChoice::QueryGenerationForecast => {
+            resp = ENTSOE_CLIENT.query_generation_forecast(START_TIME, END_TIME, AREA, None);
+        }
+        QueryChoice::QueryWindAndSolarForecast => {
+            resp = ENTSOE_CLIENT.query_wind_and_solar_forecast(START_TIME, END_TIME, AREA, None);
+        }
+        QueryChoice::QueryGeneration => {
+            resp = ENTSOE_CLIENT.query_generation(START_TIME, END_TIME, AREA);
+        }
+    };
+
+    match choice_data {
+        DataChoice::WriteToCsvFile => {
+            HelpFuncs::write_results_csv("results.csv", ";", resp.unwrap())
+        }
+        DataChoice::WriteToJsonFile => {
+            println!("Not implemented yet!")
+        }
+        DataChoice::ShowAsGraph => {
+            println!("Not implemented yet!")
+        }
+    };
 }
